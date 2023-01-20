28e37a240f465436a44da989949b534438578be2
a-mar92 <57841204+a-mar92@users.noreply.github.com>
2023-01-20 10:26:07 +0100

    Add files via upload

---
+++ Serializacja/src/Kredki.java
@@ -0,0 +1,5 @@
+import java.io.Serializable;
+
+public class Kredki implements Serializable {
+    String kolor = "różowy";
+}
--- Serializacja/src/Pudelko.java
+++ Serializacja/src/Pudelko.java
@@ -1,15 +1,16 @@
 import java.io.Serializable;
 
 public class Pudelko implements Serializable {
+    Kredki kredki = new Kredki();
 
     private int szerokosc;
     private int wysokosc;
 
-    public void setSzerokosc(int szerokosc){
-        this.szerokosc=szerokosc;
+    public void setSzerokosc(int szerokosc) {
+        this.szerokosc = szerokosc;
     }
 
     public void setWysokosc(int wysokosc) {
-        this.wysokosc=wysokosc;
+        this.wysokosc = wysokosc;
     }
 }
